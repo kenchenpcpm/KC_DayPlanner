19c495893c92c5fe45beaa07d42fefa0310d66c9
Ken Chen <kenchenpcpm@gmail.com>
2021-03-05 18:13:16 -0800

    js functions bracket aded

---
+++ Develop/dayplanner.js
@@ -0,0 +1,24 @@
+var hour8 = $('#8');
+var hour9 = $('#9');
+var hour10 = $('#10');
+var hour11 = $('#11');
+var hour12 = $('#12');
+var hour1 = $('#1');
+var hour2 = $('#2');
+var hour3 = $('#3');
+var hour4 = $('#4');
+var hour5 = $('#5');
+var hour6 = $('#6');
+var time = period();
+
+function savePlan() {
+
+}
+savePlan();
+
+
+function tenses() {
+
+}
+tenses();
+
